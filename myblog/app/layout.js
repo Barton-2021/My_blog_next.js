61221f13bc9831f79a444137b6c1e0cf36bf9b0c
Barton-2021 <129942228+Barton-2021@users.noreply.github.com>
2025-08-12 00:18:20 +0800

    first commit

--- myblog/app/layout.js
+++ myblog/app/layout.js
@@ -1,19 +1,24 @@
+/*
+ * @Author: Chen_陈工
+ * @Date: 2025-08-11 22:50:32
+ * @LastEditors: Do not edit
+ * @LastEditTime: 2025-08-11 23:36:32
+ * @FilePath: \My_blog_next.js\myblog\app\layout.js
+ */
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["400","500","600","700"]
+  subsets: ["latin"],weight:["400","500","600","700"]
 });
 
 const ovo = Ovo({
-  subsets: ["latin"],
-  weight: ["400"]
+  subsets: ["latin"],weight:["400"]
 });
 
 export const metadata = {
-  title: "欢迎来到陈工的世界",
-  description: "welcome to Chen's world",
+  title: "欢迎光临陈工的世界",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
--- myblog/app/page.js
+++ myblog/app/page.js
@@ -1,11 +1,7 @@
-'use client'
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
     <>
-      <Navbar/>
+
     </>
   );
 }
